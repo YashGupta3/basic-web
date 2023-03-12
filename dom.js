1526c7a8181eb24c9f42053761d7c336b19c7c47
YashGupta3 <y.yashgupta03@gmail.com>
2023-03-12 16:51:07 +0530

    assignment 2.3 complete

--- dom.js
+++ dom.js
@@ -5,16 +5,16 @@
 // console.log(document.forms);
 // console.log(document.links);
 // console.log(document.images);
-let header = document.getElementById('header-title')
-console.log(header);
+// let header = document.getElementById('header-title')
+// console.log(header);
 // header.textContent = "Text Content Changed"
 // header.innerText = "Yash"
 // // header.innerHTML = "<h6>Yash</h6>"
 // header.style.border = "solid 2px red"
 
 // Get Element by Class Names
-// let item = document.getElementsByClassName("list-group-item")
-// console.log(item);
+let item = document.getElementsByClassName("list-group-item")
+console.log(item);
 // // item[0].style.color = "red"
 // for (i=0; i<item.length; i++){
 //     item[i].style.color = "red"
