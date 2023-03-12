ac2ec0e2849b73938785fdae5754c1d9bfd36677
YashGupta3 <y.yashgupta03@gmail.com>
2023-03-12 18:24:14 +0530

    assignment 2.6 complete

--- dom.js
+++ dom.js
@@ -31,3 +31,14 @@ for(let i=0; i<l.length; i++){
 
 // Query Selector
 // let header = document.querySelector("#main-header");
+// let item2 = document.querySelector(".list-group-item:nth-child(2)")
+// item2.style.backgroundColor = "green"
+
+// let item3 = document.querySelector(".list-group-item:nth-child(3)")
+// // item3.style.display = "none"
+
+let odd = document.querySelectorAll("li:nth-child(odd)")
+
+for(let i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = "green"
+}
